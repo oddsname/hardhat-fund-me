d71e1eaa474f7fa5fb3ef1f562500363a0395e54
oddsname <vadimich2012@mail.ru>
2023-05-28 14:15:57 +0300

    added scripts to interact with our deployed smartcontracts!

---
+++ scripts/fund.js
@@ -0,0 +1,21 @@
+const {getNamedAccounts, ethers} = require('hardhat')
+
+async function main() {
+    const { deployer } = await getNamedAccounts();
+    const fundMe = await ethers.getContract("FundMe", deployer);
+    console.log("Deploying contract...");
+
+    const eth = ethers.utils.parseEther('0.1');
+
+    const txResponse = await fundMe.fund({ value: eth });
+    const txReceipt = await txResponse.wait(1);
+
+    console.log("Funded!");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.log(err);
+        process.exit(1);
+    })
---
+++ scripts/withdraw.js
@@ -0,0 +1,17 @@
+const {getNamedAccounts, ethers} = require('hardhat')
+
+async function main() {
+    const { deployer } = await getNamedAccounts();
+    const fundMe = await ethers.getContract("FundMe", deployer);
+    console.log("Deploying contract...");
+    const txResponse = await fundMe.withdraw();
+    const txReceipt = await txResponse.wait(1);
+    console.log("Withdraw!");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.log(err);
+        process.exit(1);
+    })
